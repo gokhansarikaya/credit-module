eb82a17a00fd36dd8fd2d13deeca2040e89d7c28
Gokhan Sarikaya <gokhansrky@gmail.com>
2024-12-28 13:18:35 +0300

    Jpa spring libraries hae been added

--- loan-service/loan-container/src/main/java/com/credit/module/loan/service/domain/LoanServiceApplication.java
+++ loan-service/loan-container/src/main/java/com/credit/module/loan/service/domain/LoanServiceApplication.java
@@ -2,10 +2,11 @@ package com.credit.module.loan.service.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableJpaRepositories(basePackages = {"com.credit.module.loan.service.dataaccess"})
-//@EntityScan(basePackages = {........})
+@EnableJpaRepositories(basePackages = {"com.credit.module.loan.service.dataaccess"})
+@EntityScan(basePackages = {"com.credit.module.loan.service.dataaccess"})
 @SpringBootApplication(scanBasePackages = "com.credit.module")
 public class LoanServiceApplication {
     public static void main(String[] args) {
