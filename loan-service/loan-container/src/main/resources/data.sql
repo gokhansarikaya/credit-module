505a0610a9a819cf55e631baa437e261b1341dab
Gokhan Sarikaya <gokhansrky@gmail.com>
2024-12-30 17:53:11 +0300

    change DB Postgresql to H2

---
+++ loan-service/loan-container/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.customer(id,credit_limit, name, surname, used_credit_limit) VALUES
+    ('123e4567-e89b-42d3-a456-556642440000', 5000, 'testuser', 'testusersurname', 0);
+
+INSERT INTO public.customer(id,credit_limit, name, surname, used_credit_limit) VALUES
+    ('6d868c6f-f48e-40dd-baa7-87f7a9663214', 1000, 'testuser2', 'testusersurname2', 0);
+
+INSERT INTO public.customer(id,credit_limit, name, surname, used_credit_limit) VALUES
+    ('de42ee9c-6c02-427f-bbec-18f32751f148', 1500, 'testuser3', 'testusersurname3', 0);
